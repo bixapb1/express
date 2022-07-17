3215f210088f68973eaacf4c95cc641adab1e492
Bixapb <Bixapb@gmail.com>
2022-07-17 11:49:21 +0300

    update 2.0

--- server.js
+++ server.js
@@ -7,27 +7,27 @@ const path = require("path");
 const cors = require("cors");
 require("dotenv/config");
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const whitelist = [
   "http://localhost:3000",
   "http://localhost:8080",
   "https://app-shopex.herokuapp.com/",
 ];
-const corsOptions = {
-  origin: function (origin, callback) {
-    console.log("** Origin of request " + origin);
-    if (whitelist.indexOf(origin) !== -1 || !origin) {
-      console.log("Origin acceptable");
-      callback(null, true);
-    } else {
-      console.log("Origin rejected");
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     console.log("** Origin of request " + origin);
+//     if (whitelist.indexOf(origin) !== -1 || !origin) {
+//       console.log("Origin acceptable");
+//       callback(null, true);
+//     } else {
+//       console.log("Origin rejected");
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
 
 //Routes
 const shopsRoute = require("./routes/shops");
